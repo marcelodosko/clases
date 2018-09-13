4ed412cc2247fae4abc88646c9fec9af26d81146
Marcelo Dosko <marcelodosko@gmail.com>
2018-09-13 10:44:20 -0300

    CLASE04-20180906

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var _ = require('lodash')
-var a = _.reverse([3,2,1])
-console.log(a)
+var express = require('express')
+var request = require('request')
+var moment = require('moment')
+var app = express();
+
+
+// app.get('/', function (req, res) {
+//   request('http://www.google.com', function (err, ))
+//   res.send();
+// });
+
+app.use('/static', express.static('assets'));
+
+app.get('/', function (req, res) {
+  var html = '<h1>hello' +moment.utc();
+  html += '<img src="static/image.png" />';
+  html += '</h1>';
+  res.send(html);
+})
+
+app.listen(3003, function () {
+  console.log('Example app listening on port 3000!');
+});
