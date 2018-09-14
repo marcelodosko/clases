879e52fb211f1c262093cee64e94e093d6595e61
Marcelo Dosko <marcelodosko@gmail.com>
2018-09-14 09:25:26 -0300

    dinamic-server

--- index.js
+++ index.js
@@ -3,21 +3,15 @@ var request = require('request')
 var moment = require('moment')
 var app = express();
 
-
-// app.get('/', function (req, res) {
-//   request('http://www.google.com', function (err, ))
-//   res.send();
-// });
-
-app.use('/static', express.static('assets'));
-
-app.get('/', function (req, res) {
-  var html = '<h1>hello' +moment.utc();
-  html += '<img src="static/image.png" />';
-  html += '</h1>';
-  res.send(html);
+app.get('/test', function (req, res) {
+  var html = '<h1>test ' +moment().format('LTS') +'</h1>'
+  html += '<button id="press">Press</button>'
+  html += '<p id="p"> Texto relleno</p>'
+  html += '<script src="http://localhost:3003/static/libs/jquery.js"></script>'
+  html += '<script src="http://localhost:3003/static/test.js"></script>'
+  res.send(html)
 })
 
-app.listen(3003, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3004, function () {
+  console.log('Server Dinamic listening on port 3004!')
 });
