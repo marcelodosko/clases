339dfe9fd91a6ce6e1c318d490b0078540447c1b
Marcelo Dosko <marcelodosko@gmail.com>
2018-09-14 09:10:50 -0300

    BK-Server

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 var express = require('express')
 var request = require('request')
 var moment = require('moment')
-var app = express();
+var app = express()
 
 
-// app.get('/', function (req, res) {
-//   request('http://www.google.com', function (err, ))
-//   res.send();
-// });
-
-app.use('/static', express.static('assets'));
-
-app.get('/', function (req, res) {
-  var html = '<h1>hello' +moment.utc();
-  html += '<img src="static/image.png" />';
-  html += '</h1>';
-  res.send(html);
+app.get('/api/time', function (req, res) {
+  var now = new Date()
+  var time = {
+    timestamp: Date.now(),
+    year: now.getFullYear()
+  }
+  res.json(time)
 })
 
-app.listen(3003, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(3005, function () {
+  console.log('API REST listening on port 3005!')
+})
