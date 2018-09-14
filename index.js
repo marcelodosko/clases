b2d1b36bfde59e78c2978202590380084c4ca0d9
Marcelo Dosko <marcelodosko@gmail.com>
2018-09-14 09:23:53 -0300

    static-server

--- index.js
+++ index.js
@@ -3,21 +3,8 @@ var request = require('request')
 var moment = require('moment')
 var app = express();
 
-
-// app.get('/', function (req, res) {
-//   request('http://www.google.com', function (err, ))
-//   res.send();
-// });
-
 app.use('/static', express.static('assets'));
 
-app.get('/', function (req, res) {
-  var html = '<h1>hello' +moment.utc();
-  html += '<img src="static/image.png" />';
-  html += '</h1>';
-  res.send(html);
-})
-
 app.listen(3003, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Static Server listening on port 3003!');
 });
