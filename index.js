04bd56c32d7e7cdee53a71ad3387e7364efa4fa6
Marcelo Dosko <marcelodosko@gmail.com>
2018-09-17 16:02:01 -0300

    sin nada

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var request = require('request')
 var parser = require('ua-parser-js')
 var app = express();
 
-app.get('/', function (req, res) {
-  var ua = parser(req.headers['user-agent']);
-  if (ua.os.name === 'Android') {
-   res.redirect("market://details?id=com.flashratings.hodl")
-  }
-  if (ua.os.name == 'iOS') {
-    res.redirect("itms-apps://itunes.apple.com/us/app/hodl-real-time-cryptocurrency/id1253668876")
-  }
-})
+// app.get('/', function (req, res) {
+//   var ua = parser(req.headers['user-agent']);
+//   if (ua.os.name === 'Android') {
+//    res.redirect("market://details?id=com.flashratings.hodl")
+//   }
+//   if (ua.os.name == 'iOS') {
+//     res.redirect("itms-apps://itunes.apple.com/us/app/hodl-real-time-cryptocurrency/id1253668876")
+//   }
+// })
 
 app.get('/test', function (req, res) {
   var html = '<html><head></head><body><h1>TEST HEROKU</h1></body></html>'
